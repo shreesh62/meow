7619437f211ac6ee66b57259824e4f36463ce3e4
shreesh <lolshreesh@gmail.com>
2026-01-24 15:27:28 +0530

    ggbg

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -34,8 +34,6 @@ class ErrorBoundary extends React.Component {
     return this.props.children;
   }
 }
-
-import React from 'react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
